31d887cdfd3efe6a2de39364ac4792a68399a007
ruffin hounsounnon <ruffinhounsounnon@gmail.com>
2023-04-29 14:59:07 +0100

    up color

--- 0x14-mysql/helpers/steps_replica.sql
+++ 0x14-mysql/helpers/steps_replica.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #Steps to establish replication process between servers
-replica_server_ip=3.227.3.18
-source_server_ip=3.235.21.36 
+replica_server_ip=34.227.92.198
+source_server_ip=100.25.134.109
 sudo ufw allow from $replica_server_ip to any port 3306
 sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
 bind-address = $source_server_ip
